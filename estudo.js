694d9fd5bdb948bbde46718d28ebc5cc55a69d18
felipe2221 <110102159+felipe2221@users.noreply.github.com>
2022-10-05 09:11:18 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,7 +1,7 @@
-let tabuada =4;
+let tabuada = 4;
 
 function escreva(){
-    document.write("tabuada do " + tabuada + "</h1=");
+    document.write("<h1>Tabuada do " + tabuada + "</h1>");
     document.write(tabuada + " x 1 = " + (tabuada*1)+"<br>");
     document.write(tabuada + " x 2 = " + (tabuada*2)+"<br>");
     document.write(tabuada + " x 3 = " + (tabuada*3)+"<br>");
@@ -12,5 +12,21 @@ function escreva(){
     document.write(tabuada + " x 8 = " + (tabuada*8)+"<br>");
     document.write(tabuada + " x 9 = " + (tabuada*9)+"<br>");
     document.write(tabuada + " x 10 = " + (tabuada*10)+"<br>");
+}
+
+function minhaTabuada(){
+    for(let i=1; i <= 10; i++){
+        document.write("Tabuada do " + i + "<br>");
+        for(let j = 1; j <= 10; j++){
+            document.write(i + " x " + j + " = " + (j*i)+"<br>");
+        }
+        document.write("<br>");
+    }
+}
+
+function quadrado(){
+    for(let i = 2; i <= 1000; i++){
+            document.write("O quadrado de " + i + " é " + (i*i) + "<br>");
+    }
 }
 
